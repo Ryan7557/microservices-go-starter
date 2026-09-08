0f97e68e92f6fac80b3da017c5876ff00d35f4ce
Ryan Muendesi <muendesitawana@gmail.com>
2026-09-08 20:40:36 +0200

    Add QueueConsumer implementation and related cache files
    - Introduced a new QueueConsumer struct in shared/messaging/queue_consumer.go to handle message consumption from RabbitMQ.
    - Implemented Start method to consume messages and send them to connected clients via ConnectionManager.
    - Added necessary cache files for Go build artifacts.

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -145,6 +145,16 @@ func (r *RabbitMQ) setupExchangesAndQueues() error {
 		return err
 	}
 
+	if err := r.declareAndBindQueue(
+		DriverCmdTripRequestQueue,
+		[]string{
+			contracts.DriverCmdTripRequest,
+		},
+		TripExchange,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
 

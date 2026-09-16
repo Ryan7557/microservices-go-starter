7ff3fc21ea473dd0580a057333504d7b16666de5
Ryan Muendesi <muendesitawana@gmail.com>
2026-09-16 12:14:08 +0200

    Add new cache files for Go build artifacts
    - Introduced multiple new cache files in the .cache/go-build directory.
    - Each file corresponds to a unique build artifact with versioning and hash information.
    - These changes optimize the build process by caching compiled results.

--- services/payment-service/cmd/main.go
+++ services/payment-service/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"ride-sharing/services/payment-service/internal/events"
 	"ride-sharing/services/payment-service/internal/infrastucture/stripe"
 	"ride-sharing/services/payment-service/internal/service"
 	"ride-sharing/services/payment-service/pkg/types"
@@ -50,14 +51,16 @@ func main() {
 	// Service
 	svc := service.NewPaymentService(paymentProcessor)
 
-	log.Println(svc)
-
 	// RabbitMQ connection
 	rabbitmq, err := messaging.NewRabbitMQ(rabbitMqURI)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rabbitmq.Close()
+
+	// Trip Consumer
+	tripConsumer := events.NewTripConsumer(rabbitmq, svc)
+	go tripConsumer.Listen()
 
 	log.Println("Starting RabbitMQ connection")
 

b7247b957974aee1831c36569a38f1d2b9938a2d
Ryan Muendesi <muendesitawana@gmail.com>
2026-09-24 15:25:39 +0200

    Refactor code structure and optimize performance across multiple modules

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"ride-sharing/shared/db"
 	"ride-sharing/shared/env"
 	"ride-sharing/shared/messaging"
 	"ride-sharing/shared/tracing"
@@ -41,10 +42,19 @@ func main() {
 	defer cancel()
 	defer sh(ctx)
 
+	// Initialize MongoDB
+	mongoClient, err := db.NewMongoClient(ctx, *db.NewMongoDefaultConfig())
+	if err != nil {
+		log.Fatalf("Failed to initialize MongoDB, err: %v", err)
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	mongoDb := db.GetDatabase(mongoClient, db.NewMongoDefaultConfig())
+
 	rabbitMqURI := env.GetString("RABBITMQ_URI", "amqp://guest:guest@rabbitmq:5672/")
 
-	inmemRepo := repository.NewInmemRepository()
-	svc := service.NewTripService(inmemRepo)
+	mongoDBRepo := repository.NewMongoRepository(mongoDb)
+	svc := service.NewTripService(mongoDBRepo)
 
 	go func() {
 		sigCh := make(chan os.Signal, 1)
--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8081';
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? 'ws://localhost:8081/ws';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://34.35.18.58:8081';
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? 'ws://34.35.18.58:8081/ws';
